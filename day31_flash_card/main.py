b0caf96471afef781dd7446be48d0a3d102a2e8f
devilmarti@abv.bg <devilmarti@abv.bg>
2023-06-17 23:54:57 +0300

    new flash card project with tkinter

---
+++ day31_flash_card/main.py
@@ -0,0 +1,6 @@
+import tkinter
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = tkinter.Tk()
+window.minsize(width=500,height=400)
---
+++ day31_flash_card_tkinter/main.py
@@ -0,0 +1,57 @@
+from tkinter import *
+import pandas
+import random
+import time
+BACKGROUND_COLOR = "#B1DDC6"
+#get data from file with pandas
+data = pandas.read_csv('./data/french_words.csv')
+df = pandas.DataFrame(data)
+dict = df.to_dict(orient="records")
+current_card = {}
+
+#make functions for clicking right and wrong choice buttons
+
+
+def flip_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(dict)
+    canvas.itemconfig(canvas_image,image=canvas_img)
+    canvas.itemconfig(canvas_word_text,text=current_card['French'])
+    canvas.itemconfig(canvas_title_text,text='French')
+    flip_timer = window.after(3000, func=next_card)
+
+def next_card():\
+
+    canvas.itemconfig(canvas_title_text,text='English',fill='white')
+    canvas.itemconfig(canvas_word_text,text=current_card['English'] ,fill='white')
+    canvas.itemconfig(canvas_image,image=canvas_back_img)
+
+window = Tk()
+window.minsize(height=400,width=500)
+window.config(padx=50,pady=50,background=BACKGROUND_COLOR)
+
+#Create main cards
+flip_timer = window.after(3000, func=next_card)
+
+canvas = Canvas(width=800,height=526)
+canvas_img = PhotoImage(file='./images/card_front.png')
+canvas_back_img = PhotoImage(file='./images/card_back.png')
+canvas_image = canvas.create_image(400,263,image=canvas_img)
+canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
+canvas_title_text = canvas.create_text(400,150,text='',font=('Ariel',40,'italic'))
+canvas_word_text = canvas.create_text(400,263,text='',font=('Ariel',60,'bold'))
+canvas.grid(column=0,row=0,columnspan=2)
+
+#Create buttons
+x_button_img = PhotoImage(file='./images/wrong.png')
+x_button = Button(image=x_button_img,command=flip_card)
+x_button.grid(column=0,row=1)
+
+right_button_img = PhotoImage(file='./images/right.png')
+right_button = Button(image=right_button_img,command=flip_card)
+right_button.grid(column=1,row=1)
+
+flip_card()
+
+window.mainloop()
